485fbd7ffdf0706cc7b598904b980e9c0be47ff2
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-24 19:15:37 +0200

    conerting fetching to custom hook useFetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React , {useState,useEffect} from 'react';
 import {  useForm  } from "./useFrom";
+import {useFetch} from './useFetch';
 function App() {
   
    const [values, handleChange] = useForm({email:'',password:''});
@@ -18,16 +19,12 @@ function App() {
   //   return ()=>{};//this is a cleanup function "replace to componentWillUnmount"
   // },[email]);//array of dependencies if anyone of them changed useEffect will run the callback function
    
- const [state,setState] = useState({data:null,loading:true});
-  useEffect(()=>{
-       fetch('http://numbersapi.com/43/trivia')
-       .then(x=>x.text()).then(y=> setState({data:y,loading:false}));
-  });
+   const state = useFetch('http://numbersapi.com/43/trivia');
 
 
   return (
     <div className="App">
-      {state.loading?'Loading.......':state.data};
+      {state.loading?'Loading.......':state.data}
       <input  type="text" name="email" value={values.email} onChange={handleChange}  />
       <input type="password" name="password" value={values.password}  onChange={handleChange} />
         
---
+++ src/useFetch.js
@@ -0,0 +1,13 @@
+import  React  , {useState,useEffect} from 'react';
+
+export const useFetch = (url) => {
+     const [state,setState] = useState({data:null, loading:true});
+     useEffect(() => {
+         setState({date:null,loading:true})
+         fetch(url)
+         .then(x=>x.text())
+         .then(y=>setState({data:y,loading:false}));
+     },[url] );
+
+     return state;
+}
