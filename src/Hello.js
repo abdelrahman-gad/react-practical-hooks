359dbcb97f007fcd6e194adcb454baf276fe1866
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-24 20:51:23 +0200

    using useLayoutEffect to measuer the chenge of dimesions of specific dev

--- src/Hello.js
+++ src/Hello.js
@@ -5,15 +5,15 @@ function Hello(){
     const  [count , setCount] = useState(()=>JSON.parse(localStorage.getItem('count'))||0); 
     const  divRef = useRef();
     const state = useFetch(`http://numbersapi.com/${count}/trivia`);
- 
+    const [rect,setRect] = useState({});
     useEffect(() => {
       localStorage.setItem('count',JSON.stringify(count));
     }, [count]);
  
     useLayoutEffect(() => {
         console.log('hello');
-      console.log(divRef.current.getBoundingClientRect());
-       
+        // console.log();
+        setRect(divRef.current.getBoundingClientRect())
     }, [state.data])
    const renders = useRef(0);
    console.log('renders current'+renders.current++ );
@@ -22,7 +22,7 @@ function Hello(){
        <div style={{display:'flex' ,border:'2px solid red'}} >
           <div ref={divRef} style={{ border:'2px solid green' }} > {state.loading?'Loading.......':state.data}</div>
        </div>
-     
+       <div> dimensions of the above greened bordered div {JSON.stringify(rect)} </div>
        <button onClick={()=>setCount(c => c+1 )}>+</button>
        </>  )
 }
