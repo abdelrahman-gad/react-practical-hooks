7fb478546c65db70335d0b41ee07b5456fd11b32
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-24 20:18:00 +0200

    adding explanation comments about useRef

--- src/Hello.js
+++ src/Hello.js
@@ -1,6 +1,9 @@
 import React , {useRef}  from 'react';
 
 
+// useRef does not cause yot component to re-render when it changes
+// useRef is also used to refere to a DOM element and also any another value
+
 function Hello(){
 
    const renders = useRef(0);
