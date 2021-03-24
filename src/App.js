b8a85926e444180ef48af96ef8869f90faa574b4
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-24 19:22:16 +0200

    call the fetch api whenever the count changed

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   //   console.log('render');
   // }); this will run with any change in the state
 
+  const  [count , setCount] = useState(0); 
   // useEffect(()=>{
   // // console.log('render');
   // },[]);//will run only when componentDidMount
@@ -19,12 +20,14 @@ function App() {
   //   return ()=>{};//this is a cleanup function "replace to componentWillUnmount"
   // },[email]);//array of dependencies if anyone of them changed useEffect will run the callback function
    
-   const state = useFetch('http://numbersapi.com/43/trivia');
+   const state = useFetch(`http://numbersapi.com/${count}/trivia`);
 
 
   return (
     <div className="App">
       {state.loading?'Loading.......':state.data}
+      <button onClick={()=>setCount(c => c+1 )}>+</button>
+      <br/>
       <input  type="text" name="email" value={values.email} onChange={handleChange}  />
       <input type="password" name="password" value={values.password}  onChange={handleChange} />
         
